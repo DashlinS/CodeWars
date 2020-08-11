2c62539c381d0c99d86550e3765e93f2485f7a18
Dashlin Sermeil <65864125+DashlinS@users.noreply.github.com>
2020-08-11 01:22:08 -0400

    Create removespaces.js

---
+++ Javascript/removespaces.js
@@ -0,0 +1,14 @@
+/*Simple, remove the spaces from the string, then return the resultant string.
+
+Starting Fuction 
+function noSpace(x){
+}
+
+replace method used
+.replace(/\s/g,'')
+*/
+
+function noSpace(x){
+  str = (x);
+return str.replace(/\s/g,'');
+}
