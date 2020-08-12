960097767743e12ee3533ed5c222b714953ef64c
Dashlin Sermeil <65864125+DashlinS@users.noreply.github.com>
2020-08-11 21:29:00 -0400

    Create stringrepeat.js

---
+++ Javascript/stringrepeat.js
@@ -0,0 +1,14 @@
+//Write a function called repeatString which repeats the given string src exactly count times.
+
+//repeatStr(6, "I") // "IIIIII"
+//repeatStr(5, "Hello") // "HelloHelloHelloHelloHello"
+
+//Used str.repeat() function
+
+function repeatStr(n, s) {
+  
+let (s) = string;
+let string.repeat(n);
+return.toString(s);
+  
+  }
