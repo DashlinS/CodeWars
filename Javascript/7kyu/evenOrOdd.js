6d630985f434b35ef0e3713393176b0f24be7ff9
Dashlin Sermeil <65864125+DashlinS@users.noreply.github.com>
2020-11-01 15:52:30 -0500

    Update evenOrOdd.js

--- Javascript/7kyu/evenOrOdd.js
+++ Javascript/7kyu/evenOrOdd.js
@@ -29,5 +29,5 @@ function oddOrEven(array) {
 }
 
 //Refactored
-let oddOrEven = (array) => return array.reduce((a,b) => a + b, 0) % 2 === 0 ? 'even' : 'odd'
+let oddOrEven = (array) => array.reduce((a,b) => a + b, 0) % 2 === 0 ? 'even' : 'odd'
 
