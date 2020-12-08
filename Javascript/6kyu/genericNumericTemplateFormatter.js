63e72b4481ca80a4c6cc640d372eab478acb8178
Dashlin Sermeil <65864125+DashlinS@users.noreply.github.com>
2020-12-07 23:20:34 -0500

    Update genericNumericTemplateFormatter.js

--- Javascript/6kyu/genericNumericTemplateFormatter.js
+++ Javascript/6kyu/genericNumericTemplateFormatter.js
@@ -8,12 +8,12 @@ the template might consist of other numbers, special characters or the like: you
 if the given or default string representing the number is shorter than the template, just repeat it to fill all the spaces.
 A few examples:
 
-```numericFormatter("xxx xxxxx xx","5465253289") === "546 52532 89"
+numericFormatter("xxx xxxxx xx","5465253289") === "546 52532 89"
 numericFormatter("xxx xxxxx xx") === "123 45678 90"
 numericFormatter("+555 aaaa bbbb", "18031978") === "+555 1803 1978"
 numericFormatter("+555 aaaa bbbb") === "+555 1234 5678"
 numericFormatter("xxxx yyyy zzzz") === "1234 5678 9012"
-```
+
 */
 
 function numericFormatter(template, num = '1234567890', i = 0){
