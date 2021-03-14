2b8543d1ea0eb8b0bdde7b2bf48d99b6e5b1096e
Dashlin Sermeil <65864125+DashlinS@users.noreply.github.com>
2021-03-13 23:41:50 -0500

    Update sumOfDifferenesInArray.js

--- Javascript/8kyu/sumOfDifferenesInArray.js
+++ Javascript/8kyu/sumOfDifferenesInArray.js
@@ -1,3 +1,31 @@
 /*
+Your task is to sum the differences between consecutive pairs in the array in descending order.
 
+For example:
+
+sumOfDifferences([2, 1, 10])
+Returns 9
+
+Descending order: [10, 2, 1]
+
+Sum: (10 - 2) + (2 - 1) = 8 + 1 = 9
+
+If the array is empty or the array has only one element the result should be 0 (Nothing in Haskell).
 */
+
+
+function sumOfDifferences(arr) {
+  let answerArr = []
+  if(arr.length <= 1) return 0
+  let newArr = arr.sort((a,b) => a - b)
+  for(let i = 0; i < newArr.length-1; i++){
+    let nextEle = newArr[i + 1]
+    answerArr.push(nextEle - newArr[i])
+  }
+  return answerArr.reduce((a,b) => a + b, 0)
+}
+
+//One line
+function sumOfDifferences(arr) {
+  return arr.length > 1 ? Math.max(...arr) - Math.min(...arr) : 0
+}
