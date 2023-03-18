7d1f7c157f93d3c0c19c93d247411330ee0ee0f7
Dashlin Sermeil <65864125+DashlinS@users.noreply.github.com>
2023-03-17 22:29:16 -0400

    Create consecutiveStrings.js

---
+++ Javascript/6kyu/consecutiveStrings.js
@@ -0,0 +1,46 @@
+/*
+strarr = ["tree", "foling", "trashy", "blue", "abcdef", "uvwxyz"], k = 2
+
+Concatenate the consecutive strings of strarr by 2, we get:
+
+treefoling   (length 10)  concatenation of strarr[0] and strarr[1]
+folingtrashy ("      12)  concatenation of strarr[1] and strarr[2]
+trashyblue   ("      10)  concatenation of strarr[2] and strarr[3]
+blueabcdef   ("      10)  concatenation of strarr[3] and strarr[4]
+abcdefuvwxyz ("      12)  concatenation of strarr[4] and strarr[5]
+
+Two strings are the longest: "folingtrashy" and "abcdefuvwxyz".
+The first that came is "folingtrashy" so 
+longest_consec(strarr, 2) should return "folingtrashy".
+
+In the same way:
+longest_consec(["zone", "abigail", "theta", "form", "libe", "zas", "theta", "abigail"], 2) --> "abigailtheta"
+
+*/
+
+function longestConsec(arr, k) {
+    let longest = ''
+    let newStr = ''
+    
+    if (arr.length == 0 || k > arr.length || k <= 0) return '';
+  
+    for (let i = 0; i < arr.length; i++){
+      newStr = arr.slice(i, i+k);
+      if (newStr.join('').length > longest.length ){
+        longest = newStr.join('');
+      }
+    }
+    return longest;
+}
+
+function longestConsec(strarr, k) {
+    var longest = "";
+    for(var i=0;k>0 && i<=strarr.length-k;i++){
+      var tempArray = strarr.slice(i,i+k);
+      var tempStr = tempArray.join("");
+      if(tempStr.length > longest.length){
+        longest = tempStr;
+      }
+    }
+    return longest;
+}
