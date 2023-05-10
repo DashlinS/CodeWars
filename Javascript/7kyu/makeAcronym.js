ebe4fbb6bffe3c9f21f7f4a591673408fee85511
Dashlin Sermeil <65864125+DashlinS@users.noreply.github.com>
2023-05-09 22:22:17 -0400

    Create makeAcronym.js

---
+++ Javascript/7kyu/makeAcronym.js
@@ -0,0 +1,23 @@
+/*
+Write function which takes a string and make an acronym of it.
+
+Rule of making acronym in this kata:
+
+split string to words by space char
+take every first letter from word in given string
+uppercase it
+join them toghether
+Eg:
+
+Code wars -> C, w -> C W -> CW
+Note: There will be at least two words in the given string!
+
+*/
+
+function toAcronym(inp){
+  let acro = [];
+  inp.split(' ').forEach(word => {
+    return acro.push(word.slice(0,1));
+  });
+  return acro.join('').toUpperCase();
+}
